ed094a06c726edadcf7d70efdd1090316b8255e5
Garrett Tacoronte <garretttacoronte@gmail.com>
2017-07-08 22:21:03 -0700

    Remove extraneous lessons and files.

--- src/lessons/assert.js
+++
@@ -1,9 +0,0 @@
-function assert(pass, description) {
-  if (pass) {
-    console.log('%c✔︎ ok', 'color: green', description)
-  } else {
-    console.assert(pass, description)
-  }
-}
-
-export default assert
--- src/lessons/index.js
+++ src/lessons/index.js
@@ -2,7 +2,6 @@ import './shared.scss'
 
 export { Exercise as RenderPropsExercise, Solution as RenderPropsSolution  } from './RenderProps/index.js'
 export { Exercise as RenderStatesExercise, Solution as RenderStatesSolution  } from './RenderStates/index.js'
-export { Exercise as RenderOptimizationsExercise, Solution as RenderOptimizationsSolution  } from './RenderOptimizations/index.js'
 export { Exercise as FormsExercise, Solution as FormsSolution  } from './Forms/index.js'
 export { Exercise as ContextExercise, Solution as ContextSolution  } from './Context/index.js'
 export { Exercise as StateAsPropsExercise, Solution as StateAsPropsSolution  } from './StateAsProps/index.js'
